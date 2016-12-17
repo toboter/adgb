ef4b9bc7e22376ce5cb9033b0bfb1d31cdca223f
Tobias Schmidt <t.schmidt@rubidat.de>
2016-12-17 16:17:32 +0100

    fixing image display in show view

--- app/controllers/artefacts_controller.rb
+++ app/controllers/artefacts_controller.rb
@@ -35,7 +35,7 @@ class ArtefactsController < ApplicationController
   def show
     @illustrations_url = URI.encode("#{Rails.application.secrets.media_host}/api/media/search?q=#{@artefact.illustrations.map{|i| i.name}.join(', ') }")
     @resp = Net::HTTP.get_response(URI.parse(@illustrations_url))
-    @illustrations = JSON.parse(@resp.body)
+    @illustrations = @artefact.illustrations.any? ? JSON.parse(@resp.body) : []
   end
 
   # GET /artefacts/new
