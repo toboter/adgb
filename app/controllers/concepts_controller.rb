1c0e25894faaf9261dd5b69e956043a0c240b49b
Tobias Schmidt <t.schmidt@rubidat.de>
2018-07-23 12:56:05 +0200

    adding to_json on render

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -4,7 +4,7 @@ class ConceptsController < ApplicationController
     query = params[:q]
     resp = JSON.parse(access_token.get("/api/search/concepts.json?q=#{query}").body)['concepts']
     data = resp.map{|t| { value: [t['default_label'], t['id'].to_s, t['html_url']].join(';'), name: t['default_label'], parents: t['broader'].map{|b| b['default_label'] }.join(', '), note: t['notes'].first['body'] } }
-    render json: data
+    render json: data.to_json
   end
 
 end
