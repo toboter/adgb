651ffb133e83ca3dd3eeabae76b0539d4f4235cc
Tobias Schmidt <t.schmidt@rubidat.de>
2018-09-11 15:10:27 +0200

    removing attr json

--- app/models/import.rb
+++ app/models/import.rb
@@ -4,12 +4,8 @@
 # t.timestamps
 
 class Import < ApplicationRecord
-  include AttrJson::Record
-  include AttrJson::Record::QueryScopes
 
   has_one_attached :file
   belongs_to :creator, class_name: 'User'
 
-  attr_json_config(default_container_attribute: :data)
-  attr_json :headers, Import::Header.to_type, array: true
 end
--- app/models/import/header.rb
+++ app/models/import/header.rb
@@ -1,6 +1,3 @@
 class Import::Header
-  include AttrJson::Model
-  attr_json :name, :string
-  attr_json :mapping, :string
 
 end
