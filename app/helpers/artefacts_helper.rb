2f63b048499a9007c5198c3d13ae09b1ba804881
Tobias Schmidt <t.schmidt@rubidat.de>
2019-07-16 19:06:10 +0200

    cleaning up concept data view

--- app/helpers/artefacts_helper.rb
+++ app/helpers/artefacts_helper.rb
@@ -15,4 +15,20 @@ module ArtefactsHelper
       concat content_tag :td, object
     end if object.present?
   end
+
+  def prettify_concept_hash(values,depth=0)
+    content_tag(:ul, style: "margin-left:#{depth*5}", class: 'list-unstyled') {
+      values.each do |k,v|
+        concat content_tag :li {
+          concat(content_tag :span, "#{k.humanize}: ", class: 'text-strong')
+          if v.is_a?(String) || v.is_a?(Integer)
+            concat(content_tag :span, v.to_s)
+          elsif v.is_a?(Array)
+            concat v.inspect # prettify_concept_hash(v,depth+1)
+          end
+        } if v && !k.include?('id') && !k.include?('url') && !k.include?('_at') && !k.include?('creator')
+      end
+    } if values
+  end
+
 end
